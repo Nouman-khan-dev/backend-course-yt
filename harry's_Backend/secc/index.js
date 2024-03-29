59ecc2303cf646c6a5bb2cd98a473fa8f41e6790
Nouman Khan <nomiofficial68@gmail.com>
2024-03-29 16:19:23 +0500

    lerant some node methods

---
+++ harry's_Backend/secc/index.js
@@ -0,0 +1,13 @@
+// import fs from 'fs';
+// fs.writeFile('nomi.txt', 'nomi is good boy', () =>
+//     console.log('done')
+// );
+
+// fs.readFile('nomi.txt', (e, d) => console.log(e, d.toString()));
+
+// fs.appendFile('nomi2.txt', 'nomi is a good boy too', (e, d) =>
+//     console.log(e, 'append file data: ', d)
+// );
+
+import path from 'path';
+console.log(path);
