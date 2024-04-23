6c2927e212915b26e99fe1e3097fa4bfd4810c66
Nouman Khan <nomiofficial68@gmail.com>
2024-04-23 23:29:24 +0500

    learnt about dynomic routes and ejs

--- backendSheriyans/FirstProject/index.js
+++ backendSheriyans/FirstProject/index.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const port = 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extend: true }));
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.get('/profile/:username', (req, res) => {
+    req.params.username;
+    res.send(
+        `<h1>${req.params.username} Hello Wellcome to my Profile`
+    );
 });
 
 app.listen(port, function () {
