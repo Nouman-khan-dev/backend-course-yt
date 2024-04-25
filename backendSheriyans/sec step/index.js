c74e1871308a0a8fdad13bd3240a2fd9ce12dfbe
Nouman Khan <nomiofficial68@gmail.com>
2024-04-25 16:56:25 +0500

    send data to server and show it using ejs

--- backendSheriyans/sec step/index.js
+++ backendSheriyans/sec step/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -10,7 +11,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.render('index');
+    fs.readdir(`./files`, function (err, files) {
+        console.log(files);
+        res.render('index', { files: files });
+        console.log(files)
+    });
 });
 
-app.listen(port);
+app.post('/create', (req, res) => {
+    fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`,req.body.details,(err)=>{
+        res.redirect('/')
+ 
+        err && console.log("error while create fiel : ",err)
+    })
+});
+
+app.listen(port, () => console.log('server is listeing on: ', port));
