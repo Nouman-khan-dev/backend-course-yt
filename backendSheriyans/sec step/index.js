0a60f55f71145041ce503644695ad862e10d9d4b
Nouman Khan <nomiofficial68@gmail.com>
2024-04-26 18:11:42 +0500

    added edit functionality

--- backendSheriyans/sec step/index.js
+++ backendSheriyans/sec step/index.js
@@ -23,6 +23,15 @@ app.get('/files/:filename', (req, res) => {
     })
    
 });
+app.get('/edit/:filename', (req, res) => {
+        res.render("edit",{filename:req.params.filename})
+});
+
+app.post('/edit/:filename', (req, res) => {
+    fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`,(err)=>{
+        res.redirect('/')
+    })
+});
 
 app.post('/create', (req, res) => {
     fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`,req.body.details,(err)=>{
