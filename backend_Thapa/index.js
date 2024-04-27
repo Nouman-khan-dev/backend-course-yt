30ef89708d134047ecace0d92d6f5cf6de93bb6f
Nouman Khan <nomiofficial68@gmail.com>
2024-04-27 16:17:20 +0500

    scure private data using dotEnv

--- backend_Thapa/index.js
+++ backend_Thapa/index.js
@@ -1,25 +1,27 @@
-import express, { Router } from 'express';
-import connectDb from './utils/db.js';
-import router from './router/auth.router.js';
+import express, { Router } from "express";
+import connectDb from "./utils/db.js";
+import router from "./router/auth.router.js";
 const app = express();
 const port = 3000;
 
-app.use('/', router);
-app.use('/auth', router);
-app.use('/fb', router);
+app.use("/", router);
+app.use("/auth", router);
+app.use("/fb", router);
 
 // app.get('/fb', (req, res) => {
 //     res.status(200).send('<h1>This is nomi khan"s facebook</h1>');
 // });
 
-app.listen(port, () => {
-    console.log('server is running at port ', port);
-});
+// app.listen(port, () => {
+//   console.log("server is running at port ", port);
+// });
 
-// connectDb()
-//     .then(() => {
-//         app.listen(port, () => {
-//             console.log(`server is running at: ${port}`);
-//         });
-//     })
-//     .catch((error) => console.error('Failed to connect', error));
+connectDb()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`server is running at: ${port}`);
+    });
+  })
+  .catch((error) =>
+    console.error("Failed to connect", error.message)
+  );
--- backend_Thapa/utils/db.js
+++ backend_Thapa/utils/db.js
@@ -1,16 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const uri =
-    'mongodb+srv://developernomi68:nouman12@cluster0.xrwhmrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+  "mongodb+srv://developernomi68:nouman12@cluster0.xrwhmrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 let URI =
-    'mongodb+srv://developernomi68:nouman12@cluster0.xrwhmrp.mongodb.net/';
+  "mongodb+srv://developernomi68:nouman12@cluster0.xrwhmrp.mongodb.net";
 
 const connectDb = async () => {
-    try {
-        await mongoose.connect(URI);
-        console.log('Database Connected Successfully');
-    } catch (error) {
-        console.log('Error connecting to the database: ', error);
-        process.exit(1); // Term
-    }
+  try {
+    await mongoose.connect(URI);
+    console.log("Database Connected Successfully");
+  } catch (error) {
+    console.log("Error connecting to the database: ", error);
+    process.exit(1); // Term
+  }
 };
 export default connectDb;
