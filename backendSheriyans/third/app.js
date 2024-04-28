30f06a400d3b8d953016b1dbca3da4d4baec3d7b
Nouman Khan <nomiofficial68@gmail.com>
2024-04-28 21:25:05 +0500

    working on mongodb

---
+++ backendSheriyans/third/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const userModel = require("./usermodel");
+
+app.get("/", (req, res) => {
+  res.send("hey");
+});
+app.get("/create", async (req, res) => {
+  let newUser = await userModel.create({
+    username: "Nomi",
+    email: "nomi@gmail.com",
+    password: "2,3,3,3,",
+  });
+  res.send(newUser);
+});
+
+app.listen(3000, () => {
+  console.log("your app is listening on 3000");
+});
