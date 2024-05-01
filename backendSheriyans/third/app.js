759c12dac3a2bdb22e39f4b6e8d34f7a1b88056f
Nouman Khan <nomiofficial68@gmail.com>
2024-05-01 22:52:31 +0500

    check out how to crud in mongo db

--- backendSheriyans/third/app.js
+++ backendSheriyans/third/app.js
@@ -7,11 +7,30 @@ app.get("/", (req, res) => {
 });
 app.get("/create", async (req, res) => {
   let newUser = await userModel.create({
-    username: "Nomi",
+    username: "NomiK",
     email: "nomi@gmail.com",
     password: "2,3,3,3,",
   });
   res.send(newUser);
+});
+app.get("/update", async (req, res) => {
+  const updatedUser = await userModel.findOneAndUpdate(
+    { username: "Nomi" },
+    { email: "Nouman@gamil.com" },
+    { new: true }
+  );
+  res.send(updatedUser);
+});
+
+app.get("/read", async (req, res) => {
+  const users = await userModel.find({ username: "Nomi" }); // also can use findOne() which will return an object
+  res.send(users);
+});
+app.get("/delete", async (req, res) => {
+  const users = await userModel.findOneAndDelete({
+    username: "NomiK",
+  });
+  res.send("user deleted");
 });
 
 app.get("/fb", (req, res) => {
