afb5425a3f27f281c9f57e2c48eb2c205f2282be
Nouman Khan <nomiofficial68@gmail.com>
2024-05-07 20:20:56 +0500

    create ui for users

--- backendSheriyans/fourth/app.js
+++ backendSheriyans/fourth/app.js
@@ -10,6 +10,14 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index");
 });
+app.get("/read", (req, res) => {
+  res.render("read");
+});
+app.post("/create", (req, res) => {
+  const { name, email, password } = req.body;
+  console.log(name, email, password);
+  res.send("ok");
+});
 
 app.listen(3000, function () {
   console.log("your server is listening on 3000");
