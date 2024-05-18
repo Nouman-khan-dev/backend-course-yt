6ec3d1b4039dbf1fc7141bbcda27d491feacfc05
Nouman Khan <nomiofficial68@gmail.com>
2024-05-18 20:39:22 +0500

    learnt login logout with bcrypt and jwt

--- backendSheriyans/authentic&author/app.js
+++ backendSheriyans/authentic&author/app.js
@@ -6,7 +6,7 @@ const path = require("path");
 const ejs = require("ejs");
 const bcrypt = require("bcryptjs");
 const userModel = require("./models/user.model");
-const { brotliCompress } = require("zlib");
+const jwt = require("jsonwebtoken");
 
 app.use(cookieParser());
 app.use(express.json());
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", async function (req, res) {
   const users = await userModel.find();
   res.render("index", { users });
-  console.log(users);
+  // console.log(users);
 });
 
 app.post("/create", function (req, res) {
@@ -27,25 +27,37 @@ app.post("/create", function (req, res) {
   let hash = bcrypt.hashSync(password, salt);
 
   const user = userModel.create({ name, email, password: hash, age });
+
+  let token = jwt.sign({ email }, "shhhh.");
+  res.cookie("token", token);
+
+  res.redirect("/");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.post("/login", async function (req, res) {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email: email });
+  if (!user) return res.send("user not found");
+  const isMatch = await bcrypt.compare(
+    password,
+    user.password,
+    function (err, result) {
+      if (err) return console.log(res.send("error"));
+      if (result) return res.send("success");
+    }
+  );
+  if (!isMatch) return res.send("password is incorrect");
+});
+
+app.get("/logout", function (req, res) {
+  res.clearCookie("token");
   res.redirect("/");
 });
 
 app.listen(3000, function () {
   console.log("app is listening on 3000");
 });
-
-/*
-  const myPlaintextPassword = "mysecretpassword";
-  const salt = bcryptjs.genSaltSync(10);
-  const hash = bcryptjs.hashSync(myPlaintextPassword, salt);
-
-  console.log("hashed password:", hash);
-  res.send(hash);
-  bcryptjs.compareSync(
-    "mysecretpassword",
-    hash,
-    function (err, result) {
-      console.log(result);
-    }
-  );
-*/
