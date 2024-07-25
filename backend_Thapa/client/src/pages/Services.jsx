af162e25ded3e5a29c2e5b68eff789b53be1946e
Nouman Khan <nomiofficial68@gmail.com>
2024-07-25 12:27:26 +0500

    services page ui done and render all elements

--- backend_Thapa/client/src/pages/Services.jsx
+++ backend_Thapa/client/src/pages/Services.jsx
@@ -9,15 +9,10 @@ export default function Services() {
       try {
         const response = await fetch(URL, {
           method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
         });
         if (response.ok) {
           const data = await response.json();
-          console.log("200 success, response: ", data);
           setServices(await data.services);
-          console.log("services:", services);
         } else {
           console.log("error: services data not found");
         }
@@ -34,9 +29,31 @@ export default function Services() {
       {" "}
       <div className="min-w-screen">
         <h1 className="text-center text-3xl font-bold">Services</h1>
-        <div className="my-4 border-black border-t-2">
+        <div className="my-4 border-black border-t-2 flex flex-wrap justify-center gap-y-7 gap-x-4">
           {services.map((item) => (
-            <div className="m-4 ">{item.provider}</div>
+            <article class="min-w-[300px] text-left max-w-[350px] basis[350px] flex-1 hover:animate-background rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover:bg-[length:400%_400%] hover:shadow-sm hover:[animation-duration:_4s] dark:shadow-gray-700/25">
+              <div class="rounded-[10px] bg-white px-4 !p-8 sm:p-6 dark:bg-gray-900 min-h-full">
+                <h3 class="mt-0.5 text-xl font-bold text-gray-900 dark:text-white">
+                  {item.service}
+                </h3>
+                <h2 className="mt-0.5 font-medium text-gray-900 dark:text-gray-300">
+                  {item.description}
+                </h2>
+
+                <div class="mt-4 flex flex-wrap gap-1">
+                  <span class="whitespace-nowrap text-xl font-semibold rounded-xl bg-purple-100 px-2.5 py-0.5  text-purple-600">
+                    <span className="text-gray-500">Provider:</span>{" "}
+                    {item.provider}
+                  </span>
+                </div>
+                <div className="mt-3 text-left">
+                  <span class="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-purple-600 dark:bg-purple-600 dark:text-purple-100">
+                    <span className="text-white">Price:</span>{" "}
+                    {item.price} $
+                  </span>
+                </div>
+              </div>
+            </article>
           ))}
         </div>
       </div>
--- backend_Thapa/client/tailwind.config.js
+++ backend_Thapa/client/tailwind.config.js
@@ -3,6 +3,19 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    theme: {
+      extend: {
+        animation: {
+          border: "background ease infinite",
+        },
+        keyframes: {
+          background: {
+            "0%, 100%": { backgroundPosition: "0% 50%" },
+            "50%": { backgroundPosition: "100% 50%" },
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
