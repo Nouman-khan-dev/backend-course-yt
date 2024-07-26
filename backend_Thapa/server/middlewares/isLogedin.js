8ca4fcad6f9931effd31b0e80aaa1fdd60dfdcdf
Nouman Khan <nomiofficial68@gmail.com>
2024-07-26 17:23:20 +0500

    fixed an issue

--- backend_Thapa/server/middlewares/isLogedin.js
+++ backend_Thapa/server/middlewares/isLogedin.js
@@ -3,20 +3,29 @@ export const isLogedIn = (req, res, next) => {
   const token = req.header("Authorization");
   try {
     if (!token) {
-      return res.status(401).json({ error: "Access Denied" });
-    } else {
-      const decode = jwt.verify(
-        token.replace("Bearer", "").trim(),
-        "THISISMYSECRETKEY"
-      );
-      if (decode) {
-        req.user = decode;
-      }
+      return res
+        .status(401)
+        .json({
+          error:
+            "Access Denied, Unauthorized request! No token found",
+        });
     }
+    const decoded = jwt.verify(
+      token.replace("Bearer", "").trim(),
+      "THISISMYSECRETKEY"
+    );
+    req.user = decoded;
+    next();
   } catch (error) {
-    res.status(500).json({
-      message: "Server Error while authenticating user",
-    });
+    if (error instanceof jwt.JsonWebTokenError) {
+      return res
+        .status(401)
+        .json({ message: "Invalid token", error: error });
+    } else {
+      console.error("Error while authenticating user:", error);
+      res
+        .status(500)
+        .json({ message: "Server Error while authenticating user" });
+    }
   }
-  next();
 };
